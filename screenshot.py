e332194d121f8618386fcc5b0a7ca0100657cdc1
Dave Stanton <davestanton.us@gmail.com>
2013-10-09 10:47:40 -0400

    new screenshots working via Webdriver.

--- screenshot.py
+++ screenshot.py
@@ -17,19 +17,19 @@ with contextlib.closing(webdriver.Firefox()) as driver:
         driver.get(urls[key])
         
         driver.set_window_size(1280,800)
-        driver.get_screenshot_as_file('desktop/desktop ' + key + '.png') 
+        driver.get_screenshot_as_file('new/desktop/desktop ' + key + '.png') 
     
         driver.set_window_size(768,1024)
-        driver.get_screenshot_as_file('ipad/ipad ' + key + '.png') 
+        driver.get_screenshot_as_file('new/ipad/ipad ' + key + '.png') 
     
         driver.set_window_size(320,568)
-        driver.get_screenshot_as_file('iphone/iphone ' + key + '.png')
+        driver.get_screenshot_as_file('new/iphone/iphone ' + key + '.png')
     
         driver.set_window_size(320,640)
-        driver.get_screenshot_as_file('galaxys4/galaxys4 ' + key + '.png') 
+        driver.get_screenshot_as_file('new/galaxys4/galaxys4 ' + key + '.png') 
     
         driver.set_window_size(601,906)
-        driver.get_screenshot_as_file('nexus7/nexus7 ' + key + '.png')
+        driver.get_screenshot_as_file('new/nexus7/nexus7 ' + key + '.png')
     
         driver.set_window_size(384,592)
-        driver.get_screenshot_as_file('nexus4/nexus4 ' + key + '.png')
+        driver.get_screenshot_as_file('new/exus4/nexus4 ' + key + '.png')
